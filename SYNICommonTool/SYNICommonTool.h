45cd37a2974b15d12e19201cf04d5ae7558ec4e1
zhiquan.xu <zhiquan.xu@xblgroup.com>
2020-02-10 18:07:42 +0800

    TEST

---
+++ SYNICommonTool/SYNICommonTool.h
@@ -0,0 +1,17 @@
+//
+//  SYNICommonTool.h
+//  GQComponentDemo
+//
+//  Created by zhiquan.xu on 2020/2/10.
+//  Copyright © 2020年 quangqiang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SYNICommonTool : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SYNICommonTool/SYNICommonTool.m
@@ -0,0 +1,13 @@
+//
+//  SYNICommonTool.m
+//  GQComponentDemo
+//
+//  Created by zhiquan.xu on 2020/2/10.
+//  Copyright © 2020年 quangqiang. All rights reserved.
+//
+
+#import "SYNICommonTool.h"
+
+@implementation SYNICommonTool
+
+@end
